8475650918d48d7de0ec52baaedf13a907885016
Chaitanya Deshpande <chaitanya.deshpande@avinetworks.com>
2016-07-22 16:07:30 +0530

    added auth guard

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable }             from '@angular/core';
+import { CanActivate,
+         Router,
+         ActivatedRouteSnapshot,
+         RouterStateSnapshot }    from '@angular/router';
+import { SessionService }         from '../services/index';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+	constructor(private session: SessionService, private router: Router) {}
+
+  canActivate( next:  ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+  	if (!this.session.isLoggedIn() && state.url === '/login') {
+    	return true;
+    } else if (this.session.isLoggedIn() && state.url !== '/login') {
+    	return true;
+    } else if (this.session.isLoggedIn() && state.url === '/login') {
+    	this.router.navigate(['']);
+    	return false;
+    } else {
+    	this.router.navigate(['/login']);
+	  	return false;
+    }
+  }
+}
